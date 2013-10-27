12e5317dcdf426ead8c9cb835473a377a94af02a
Bernardo Alves <bernardo.d.alves@gmail.com>
2013-10-27 17:01:12 -0200

    nova funcao

--- src/Spa/BackendBundle/Entity/Menu.php
+++ src/Spa/BackendBundle/Entity/Menu.php
@@ -91,38 +91,38 @@ class Menu
         return $this->slug;
     }
 
-  public function toAscii($text)
-{
+    public function toAscii($text)
+    {
 
-    $a = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ??';
-        $b = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr';
+            $a = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ??';
+                $b = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr';
 
-        $string = utf8_decode($text);
-        $string = strtr($string, utf8_decode($a), $b);
-       // $string = str_replace('', $type,$string);
-        $text = strtolower($string);
+                $string = utf8_decode($text);
+                $string = strtr($string, utf8_decode($a), $b);
+               // $string = str_replace('', $type,$string);
+                $text = strtolower($string);
 
-        
-  // replace non letter or digits by -
-  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+                
+          // replace non letter or digits by -
+          $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
 
-  // trim
-  $text = trim($text, '-');
+          // trim
+          $text = trim($text, '-');
 
-  // transliterate
-  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-  // lowercase
-  $text = strtolower($text);
+          // transliterate
+          $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+          // lowercase
+          $text = strtolower($text);
 
-  // remove unwanted characters
-  $text = preg_replace('~[^-\w]+~', '', $text);
+          // remove unwanted characters
+          $text = preg_replace('~[^-\w]+~', '', $text);
 
-  if (empty($text))
-  {
-    return 'n-a';
-  }
-  return $text;
-}
+          if (empty($text))
+          {
+            return 'n-a';
+          }
+          return $text;
+    }
 
 
     /**
--- src/Spa/BackendBundle/Entity/Product.php
+++ src/Spa/BackendBundle/Entity/Product.php
@@ -302,18 +302,38 @@ class Product
         return $this;
     }
 
-    public function toAscii($str, $replace=array(), $delimiter='-') {
-     if( !empty($replace) ) {
-      $str = str_replace((array)$replace, ' ', $str);
-     }
+    public function toAscii($text)
+    {
 
-     $clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
-     $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
-     $clean = strtolower(trim($clean, '-'));
-     $clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
+        $a = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ??';
+            $b = 'aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr';
 
-     return $clean;
-    }
+            $string = utf8_decode($text);
+            $string = strtr($string, utf8_decode($a), $b);
+           // $string = str_replace('', $type,$string);
+            $text = strtolower($string);
+
+            
+      // replace non letter or digits by -
+      $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+      // trim
+      $text = trim($text, '-');
+
+      // transliterate
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+      // lowercase
+      $text = strtolower($text);
+
+      // remove unwanted characters
+      $text = preg_replace('~[^-\w]+~', '', $text);
+
+      if (empty($text))
+      {
+        return 'n-a';
+      }
+      return $text;
+}
 
 
     /**
